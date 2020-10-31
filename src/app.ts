2543a7bdabc0cec8b8e93859e899472998f8f689
Veternum <hans.rhesa88@gmail.com>
2020-10-31 11:39:14 +0700

    Week 8

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,16 @@ import bodyParser from "body-parser";
 import cors from 'cors';
 import http from 'http';
 import config from './environment';
+import routes from './routes';
 
 const app = express();
 
 app.use(cors({ origin: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
+routes(app);
 
+/*
 app.get('/index', (req, res) => {
     console.log("req: ", req)
     return res.status(200).json({message: "Success!"});
@@ -29,6 +32,7 @@ app.put('/update', (req, res) => {
     console.log("req: ", req)
     return res.status(200).json({message: "Success!"});
 });
+*/
 
 const server = http.createServer(app);
 
