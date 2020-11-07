92bfcdcf50c7ec0c0e5971f1c4cb6cbc0841e00d
Veternum <hans.rhesa88@gmail.com>
2020-11-07 11:24:05 +0700

    MongoDB

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import http from 'http';
 import config from './environment';
 import routes from './routes';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -12,6 +13,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 routes(app);
 
+mongoose.connect(config.mongo_uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+const database = mongoose.connection;
+database.once("open", async () => {
+    console.log("Connected to database");
+});
+database.on("error", () => {
+    console.log("Error connecting to database");
+});
 /*
 app.get('/index', (req, res) => {
     console.log("req: ", req)
