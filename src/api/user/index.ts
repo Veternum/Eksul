745054c16d70020e9765333118dfabad173839d9
Veternum <hans.rhesa88@gmail.com>
2020-11-21 13:14:07 +0700

    Lesson Learned

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,16 +1,19 @@
 import express from 'express';
 import{
     index,
-    create,
+    register,
+    signin,
     remove,
     update
 } from './user.controller';
+import * as middleware from '../../middleware';
 
 let userRouter = express.Router();
 
-userRouter.get('/', index);
-userRouter.post('/', create);
-userRouter.delete('/:id', remove);
-userRouter.put('/:id', update);
+userRouter.post('/register', register);
+userRouter.post('/signin', signin);
+userRouter.get('/', middleware.isAuthenticated, index);
+userRouter.delete('/:id', middleware.isAuthenticated, remove);
+userRouter.put('/:id', middleware.isAuthenticated, update);
 
 export default userRouter;
